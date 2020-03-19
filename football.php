92d1d76dcc81c9b7f5ea780e1f052d98fb34d957
SkYiMlTo <gogo.bourreau@hotmail.fr>
2020-03-19 12:15:48 +0100

    Initial commit

--- football.php
+++ football.php
@@ -5,13 +5,13 @@ class football
 {
     private $name;
     private $nbOfTitles;
-    const NBEQUIPES = "Nombre d'équipe";
+    const NBEQUIPES = "Nombre d'équipe : ";
 
     static $nbEquipes = 0;
 
     static function displayNbEquipes()
     {
-        echo self::NBEQUIPES;
+        echo self::NBEQUIPES.self::$nbEquipes."<br />";
     }
 
     function display()
@@ -41,16 +41,22 @@ class football
 
     function __construct($name, $nbOfTitles)
     {
+        self::$nbEquipes++;
         $this->name = $name;
         $this->nbOfTitles = $nbOfTitles;
     }
 
     function __destruct()
     {
+        self::$nbEquipes--;
         echo "Destructor\n";
     }
 
 }
 
-$equipe = new football("Nantes", 45);
+$equipe = new football("Name1", 1);
+$equipe2 = new football("Name2", 2);
+$equipe3 = new football("Name3", 3);
+$equipe4 = new football("Name4", 4);
 $equipe->display();
+$equipe::displayNbEquipes();
